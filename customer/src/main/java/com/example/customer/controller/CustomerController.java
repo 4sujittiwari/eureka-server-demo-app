b45663d196a5aec980b77e5de249fea01b823328
Sujit Kumar <sujit.k@spotqa.com>
2019-09-11 13:27:33 +0530

    Added project

---
+++ customer/src/main/java/com/example/customer/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.example.customer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.customer.dto.Customer;
+
+@RestController
+public class CustomerController {
+
+	@GetMapping(value = "/details")
+	public Customer getDetails() {
+		
+		return new Customer("abc", "xyz", "server 1");
+	}
+
+}
